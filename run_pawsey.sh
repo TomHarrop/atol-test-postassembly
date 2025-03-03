b59b0104c04feac14238fe1697e0daedfa20871d
Tom Harrop <twharrop@gmail.com>
2025-03-03 14:56:36 +1100

    initial commit

---
+++ run_pawsey.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#SBATCH --job-name=atol_ear
+#SBATCH --time=0-02
+#SBATCH --cpus-per-task=2
+#SBATCH --ntasks=1
+#SBATCH --mem=8g
+#SBATCH --output=sm.slurm.out
+#SBATCH --error=sm.slurm.err
+
+# Dependencies
+module load python/3.11.6
+module load nextflow/24.04.3
+module load singularity/4.1.0-nohost
+
+unset SBATCH_EXPORT
+
+# Application specific commands:
+set -eux
+
+source /software/projects/pawsey1132/tharrop/atol-test-postassembly/venv/bin/activate
+
+printf "TMPDIR: %s\n" "${TMPDIR}"
+printf "SLURM_CPUS_ON_NODE: %s\n" "${SLURM_CPUS_ON_NODE}"
+
+if [ -z "${SINGULARITY_CACHEDIR}" ]; then
+	export SINGULARITY_CACHEDIR=/software/projects/pawsey1132/tharrop/.singularity
+	export APPTAINER_CACHEDIR="${SINGULARITY_CACHEDIR}"
+fi
+
+export NXF_APPTAINER_CACHEDIR="${SINGULARITY_CACHEDIR}/library"
+export NXF_SINGULARITY_CACHEDIR="${SINGULARITY_CACHEDIR}/library"
+
+# Pull the containers into the cache before trying to launch the workflow.
+# Using the latest commit to dev because of issues with staging from s3 on
+# release 0.10.0. See
+# https://github.com/sanger-tol/genomeassembly/compare/0.10.0...dev
+nextflow inspect \
+	-concretize sanger-tol/ear \
+	--input resources/ear_config_test.yaml \
+	--outdir s3://pawsey1132.atol.testpostassembly/414129_AusARG/results/ear \
+	-profile singularity,pawsey \
+	-r 7c83e83
+
+# Note, it's tempting to use the apptainer profile, but the nf-core (and some
+# sanger-tol) pipelines have a conditional `workflow.containerEngine ==
+# 'singularity'` that prevents using the right URL with apptainer.
+nextflow \
+	-log "nextflow_logs/nextflow.$(date +"%Y%m%d%H%M%S").${RANDOM}.log" \
+	run \
+	sanger-tol/ear \
+	--input resources/ear_config_test.yaml \
+	--outdir s3://pawsey1132.atol.testpostassembly/414129_AusARG/results/ear \
+	-resume \
+	-profile singularity,pawsey \
+	-r 7c83e83
