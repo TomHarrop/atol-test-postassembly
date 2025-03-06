189e212ff4419884c3c15bdb6a58b8d6dce0b9ce
Tom Harrop <twharrop@gmail.com>
2025-03-07 09:27:45 +1100

    setup genomenote

--- run_pawsey.sh
+++ run_pawsey.sh
@@ -20,6 +20,22 @@ set -eux
 
 source /software/projects/pawsey1132/tharrop/atol-test-postassembly/venv/bin/activate
 
+if [ -z "${1}" ]; then
+    printf "Usage: run_pawsey.sh <pipeline_name>\n"
+    exit 1
+fi
+
+export PIPELINE="${1}"
+printf "PIPELINE: %s\n" "${PIPELINE}"
+
+# get the pipeline version from pipeline_versions.yaml
+export PIPELINE_VERSION=$(grep "^${PIPELINE}:" resources/pipeline_versions.yaml | cut -d':' -f2 | tr -d ' ')
+if [ -z "${PIPELINE_VERSION}" ]; then
+    printf "Pipeline version for %s not found in pipeline_versions.yaml\n" "${PIPELINE}"
+    exit 1
+fi
+printf "PIPELINE_VERSION: %s\n" "${PIPELINE_VERSION}"
+
 printf "TMPDIR: %s\n" "${TMPDIR}"
 printf "SLURM_CPUS_ON_NODE: %s\n" "${SLURM_CPUS_ON_NODE}"
 
@@ -35,11 +51,11 @@ export NXF_SINGULARITY_CACHEDIR="${SINGULARITY_CACHEDIR}/library"
 # Using release 0.6.2 because dev has a bug with the "MAIN_MAPPING" workflow
 # not being defined.
 nextflow inspect \
-    -concretize sanger-tol/ear \
-    --input resources/ear_config_test.yaml \
-    --outdir s3://pawsey1132.atol.testpostassembly/414129_AusARG/results/ear \
+    -concretize "sanger-tol/${PIPELINE}" \
+    --input "resources/configs/${PIPELINE}_test.yaml" \
+    --outdir "s3://pawsey1132.atol.testpostassembly/414129_AusARG/results/${PIPELINE}" \
     -profile singularity,pawsey \
-    -r 0.6.2
+    -r "${PIPELINE_VERSION}"
 
 # Note, it's tempting to use the apptainer profile, but the nf-core (and some
 # sanger-tol) pipelines have a conditional `workflow.containerEngine ==
@@ -47,9 +63,9 @@ nextflow inspect \
 nextflow \
     -log "nextflow_logs/nextflow.$(date +"%Y%m%d%H%M%S").${RANDOM}.log" \
     run \
-    sanger-tol/ear \
-    --input resources/ear_config_test.yaml \
-    --outdir s3://pawsey1132.atol.testpostassembly/414129_AusARG/results/ear \
+    "sanger-tol/${PIPELINE}" \
+    --input "resources/configs/${PIPELINE}_test.yaml" \
+    --outdir "s3://pawsey1132.atol.testpostassembly/414129_AusARG/results/${PIPELINE}" \
     -resume \
     -profile singularity,pawsey \
-    -r 0.6.2
+    -r "${PIPELINE_VERSION}"
