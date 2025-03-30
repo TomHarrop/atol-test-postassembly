fd218e7f1b27e570f8ccd27a011abb1bf7f4d79c
Tom Harrop <twharrop@gmail.com>
2025-03-31 10:46:48 +1100

    add hic to genomenote

--- run_genomenote_pawsey.sh
+++ run_genomenote_pawsey.sh
@@ -41,12 +41,23 @@ fi
 export NXF_APPTAINER_CACHEDIR="${SINGULARITY_CACHEDIR}/library"
 export NXF_SINGULARITY_CACHEDIR="${SINGULARITY_CACHEDIR}/library"
 
-# Note, teh files were manually copied from
+# Note, the files were manually copied from
 # pawsey1132:/pawsey1132.atol.testassembly/414129_AusARG/results/ to
-# resources/414129_AusARG get the pipelin running.
+# resources/414129_AusARG get the pipeline running. The import_mapped_hic_reads
+# converts the mapped Hi-C reads from
+# pawsey1132:/pawsey1132.atol.testassembly/414129_AusARG/results/sanger_tol/414129.hifiasm.20250123/scaffolding/414129_mkdup.bam
+# to cram.
 # sanger_tol/414129.hifiasm.20250123/scaffolding/yahs/out.break.yahs/out_scaffolds_final.fa
 # was manually renamed to GCA_032191835.1 because the genomenote pipeline wants
 # an accession number.
+
+snakemake \
+    --profile profiles/pawsey_v8 \
+    --retries 1 \
+    --cores 12 \
+    --notemp \
+    --local-cores "${SLURM_CPUS_ON_NODE}"
+    -s workflow/import_mapped_hic_reads.smk
 
 # Pull the containers into the cache before trying to launch the workflow.
 # Using release 0.6.2 because dev has a bug with the "MAIN_MAPPING" workflow
